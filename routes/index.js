77fc81d52a752e9ce63a5a8243d2ae122f327d2a
TristanVarewijck <tristanconnect4@outlook.com>
2022-06-28 13:21:49 +0200

    database connection

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,46 @@ let express = require("express");
 let router = express.Router();
 const fs = require("fs");
 const axios = require("axios").default;
-
 const { cryptoSymbol } = require("crypto-symbol");
 const { nameLookup } = cryptoSymbol({});
+require("dotenv").config();
+
+// DATABASE
+// connection string
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@binancechatcluster.szvgg.mongodb.net/?retryWrites=true&w=majority`;
+
+// settings
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("Connected correctly to server");
+    const db = client.db("test");
+    const col = db.collection("coins");
+
+    let personDocument = {
+      name: { first: "Alan", last: "Turing" },
+      birth: new Date(1912, 5, 23), // May 23, 1912
+      death: new Date(1954, 5, 7), // May 7, 1954
+      contribs: ["Turing machine", "Turing test", "Turingery"],
+      views: 1250000,
+    };
+
+    await col.insertOne(personDocument);
+  } catch (err) {
+    console.log(err.stack);
+  } finally {
+    await client.close();
+  }
+}
+
+run().catch(console.dir);
 
 router.get("/", function (req, res, next) {
   res.redirect("/room?id=btc");
