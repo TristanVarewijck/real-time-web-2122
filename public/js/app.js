e02ee8e227bea28c6b0252249e636ef6d6a50846
TristanVarewijck <tristanconnect4@outlook.com>
2022-07-23 19:53:37 +0200

    emojipicker

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,23 @@
 // This js file is only for functions unrelated to sockets or binance websockets
 import { getSymbols } from "./partials/symbols.js";
+// import { emojis } from "./partials/emojiPicker.js";
 // insert all current symbols into token select-list
-
 getSymbols();
+// emojis();
+
+const emojiPickerContainer = document.querySelector(".emoji-picker-container");
+const emojiPicker = document.querySelector(".emoji-picker");
+
+emojiPicker.addEventListener("emoji-click", (event) =>
+  console.log(event.detail)
+);
+
+const emojiButton = document.querySelector(".emoji-button");
+emojiButton.addEventListener("click", (e) => {
+  emojiPickerContainer.classList.toggle("hidden");
+  console.log(emojiPickerContainer);
+});
+
+emojiPicker.addEventListener("emoji-click", (event) =>
+  console.log(event.detail)
+);
---
+++ public/js/partials/emojiPicker.js
@@ -0,0 +1,5 @@
+// document
+//   .querySelector("emoji-picker")
+//   .addEventListener("emoji-click", (event) => console.log(event.detail));
+
+// export { getSymbols };
